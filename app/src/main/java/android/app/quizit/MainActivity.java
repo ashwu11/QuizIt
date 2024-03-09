926c01d42d8e653c0aaff89e6f79e2f02b074c41
Ashley Wu <ashleywu2004@gmail.com>
2024-03-08 21:45:41 -0800

    implement quiz functionality and add score display

--- app/src/main/java/android/app/quizit/MainActivity.java
+++ app/src/main/java/android/app/quizit/MainActivity.java
@@ -14,11 +14,14 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView totalQuestionsTextView;
     TextView questionTextView;
+    TextView scoreTextView;
     Button ansA, ansB, ansC, ansD;
     Button enterButton;
 
     int score = 0;
     int totalQuestions = QuizEntry.questions.length;
+    int currentQuestion = 0;
+    String selection = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         totalQuestionsTextView = findViewById(R.id.total_questions);
         questionTextView = findViewById(R.id.question);
+        scoreTextView = findViewById(R.id.score);
         ansA = findViewById(R.id.ans_A);
         ansB = findViewById(R.id.ans_B);
         ansC = findViewById(R.id.ans_C);
@@ -44,10 +48,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ansC.setOnClickListener(this);
         ansD.setOnClickListener(this);
         enterButton.setOnClickListener(this);
+
+        totalQuestionsTextView.setText("Total Questions: " + totalQuestions);
+        scoreTextView.setText("Score: " + score);
+
+        loadQuestion();
     }
 
     @Override
     public void onClick(View v) {
+        resetBackground();
 
+        Button clicked = (Button) v;
+        if (clicked.getId() == R.id.enter_button) {
+            if (selection.equals(QuizEntry.correctAnswers[currentQuestion])) {
+                score++;
+                scoreTextView.setText("Score: " + score);
+            }
+            currentQuestion++;
+            loadQuestion();
+        } else {
+            selection = clicked.getText().toString();
+            clicked.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_light));
+        }
+    }
+
+    void resetBackground() {
+        ansA.setBackgroundColor(getResources().getColor(android.R.color.white));
+        ansB.setBackgroundColor(getResources().getColor(android.R.color.white));
+        ansC.setBackgroundColor(getResources().getColor(android.R.color.white));
+        ansD.setBackgroundColor(getResources().getColor(android.R.color.white));
+    }
+
+    void loadQuestion() {
+        questionTextView.setText(QuizEntry.questions[currentQuestion]);
+        ansA.setText(QuizEntry.options[currentQuestion][0]);
+        ansB.setText(QuizEntry.options[currentQuestion][1]);
+        ansC.setText(QuizEntry.options[currentQuestion][2]);
+        ansD.setText(QuizEntry.options[currentQuestion][3]);
     }
 }
