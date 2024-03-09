01a22dd121bb609c510f73076888804a1d92de91
Ashley Wu <ashleywu2004@gmail.com>
2024-03-08 21:15:19 -0800

    create quiz questions and add views to MainActivity

--- app/src/main/java/android/app/quizit/MainActivity.java
+++ app/src/main/java/android/app/quizit/MainActivity.java
@@ -1,6 +1,9 @@
 package android.app.quizit;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView totalQuestionsTextView;
+    TextView questionTextView;
+    Button ansA, ansB, ansC, ansD;
+    Button enterButton;
+
+    int score = 0;
+    int totalQuestions = QuizEntry.questions.length;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        totalQuestionsTextView = findViewById(R.id.total_questions);
+        questionTextView = findViewById(R.id.question);
+        ansA = findViewById(R.id.ans_A);
+        ansB = findViewById(R.id.ans_B);
+        ansC = findViewById(R.id.ans_C);
+        ansD = findViewById(R.id.ans_D);
+        enterButton = findViewById(R.id.enter_button);
+
+        ansA.setOnClickListener(this);
+        ansB.setOnClickListener(this);
+        ansC.setOnClickListener(this);
+        ansD.setOnClickListener(this);
+        enterButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
---
+++ app/src/main/java/android/app/quizit/QuizEntry.java
@@ -0,0 +1,22 @@
+package android.app.quizit;
+
+public class QuizEntry {
+    public static String[] questions = {
+            "How many evolutions does Eevee have?",
+            "Which pokemon has the special ability to devolve?",
+            "Which pokemon has time controlling abilities?"
+    };
+
+    public static String[][] options = {
+            {"Five", "Eight", "Four", "Seven"},
+            {"Pikachu", "Bulbasaur", "Slowbro", "Rhydon"},
+            {"Empoleon", "Palkia", "Darkrai", "Dialga"}
+    };
+
+    public static String[] correctAnswers = {
+            "Eight",
+            "Slowbro",
+            "Dialga"
+    };
+
+}
