08f68b0c585049251c98779bcd03b89ee5879acc
Ashley Wu <ashleywu2004@gmail.com>
2024-03-08 22:04:55 -0800

    add quiz questions

--- app/src/main/java/android/app/quizit/QuizEntry.java
+++ app/src/main/java/android/app/quizit/QuizEntry.java
@@ -4,19 +4,25 @@ public class QuizEntry {
     public static String[] questions = {
             "How many evolutions does Eevee have?",
             "Which pokemon has the special ability to devolve?",
-            "Which pokemon has time controlling abilities?"
+            "Which pokemon has time controlling abilities?",
+            "Who is the only Fire/Water dual-type Pokémon?",
+            "What is the meaning of the word Pokemon in Japanese?"
     };
 
     public static String[][] options = {
             {"Five", "Eight", "Four", "Seven"},
             {"Pikachu", "Bulbasaur", "Slowbro", "Rhydon"},
-            {"Empoleon", "Palkia", "Darkrai", "Dialga"}
+            {"Empoleon", "Palkia", "Darkrai", "Dialga"},
+            {"Volcanion", "Charizard", "Blaziken", "Infernape"},
+            {"Pocket animals", "Pocket monsters", "Pet monsters", "Pet animals"}
     };
 
     public static String[] correctAnswers = {
             "Eight",
             "Slowbro",
-            "Dialga"
+            "Dialga",
+            "Volcanion",
+            "Pocket monsters"
     };
 
 }
